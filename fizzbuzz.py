2718ef95b0e393059be0de03f5f555c2f0c4ebb0
Connor Bingham <142693389+connorbingham92@users.noreply.github.com>
2023-10-29 14:24:14 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,14 @@
-# add your code here
+for i in range(1,101):
+    #check to see if each number is divisible by both 3 and 5
+    if(i%3==0 and i%5==0):
+        print("FizzBuzz")
+    #check to see if number is divisible by 3
+    elif(i%3==0):
+        print("Fizz")
+    #check to see if number is divisible by 5
+    elif(i%5==0):
+        print("Buzz")
+    #when number is not divisible by either, print i
+    else:
+        print(i) # add your code here
 
